476fc6e456dff80720abbd2d001497810f75c423
haihua1994 <1259888246@qq.com>
2018-12-03 10:06:17 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+const app=getApp()
+
+Page({
+  data: {
+    slides:null,
+    entities: null
+  },
+  onLoad(){
+    this.setData({
+      slides:app.globalData.slides,
+      entities: app.globalData.vehicles
+    })
+  }
+})
